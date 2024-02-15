232fcb5692986ce393ea3077efb1b00c4136762e
BT_1N3_21 <mm@>
2024-02-15 09:24:16 -0500

    first

---
+++ src/Movie.java
@@ -0,0 +1,14 @@
+public class Movie {
+    private String title = "";
+    private String cast = "";
+
+    private String director = "";
+
+    private String overview = "";
+    private int runtime = 0;
+
+    private double userRating = 0;
+    public Movie(){
+
+    }
+}
---
+++ src/MovieCollection.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class MovieCollection {
+    public void imports(){
+        ArrayList<Movie> movieList = new ArrayList<Movie>();
+        ArrayList<Integer> ratings = new ArrayList<Integer>();
+        ArrayList<String> cast = new ArrayList<String>();
+        try {
+            int add = 0;
+            File myFile = new File("src//movies_data.csv");
+            Scanner fileScanner = new Scanner(myFile);
+            fileScanner.nextLine();
+            while (fileScanner.hasNext()) {
+                String data = fileScanner.nextLine();
+                int i = 0;
+                int next = 0;
+                String sub = "";
+                while(i >= 0){
+                    i = data.indexOf(",");
+                    if(i > 0){
+                        String s = data.substring(0, i);
+                        add += Integer.parseInt(s);
+                        data = data.substring(i + 1);
+                    }
+                    else {
+
+                    }
+                }
+                ratings.add(add);
+                add = 0;
+            }
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+
+    }
+}
